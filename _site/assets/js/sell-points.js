167d83d76f97d7a214750c85d8197971cbbc4cab
h5avlov <h5avlov@abv.bg>
2020-09-22 01:18:28 +0300

    Final date pushed back

---
+++ _site/assets/js/sell-points.js
@@ -0,0 +1,15 @@
+( function() {
+	// Initializes main slider
+	$( '.page-head .head-list' ).bxSlider( { 
+		// auto: true, 
+		// autoStart: true, 
+		// infiniteLoop: true, 
+		mode: "vertical", 
+		controls: false, 
+		adaptiveHeight: true,  
+		pause: 7000, 
+		autoHover: true, 
+		stopAutoOnClick: true 
+	} ); 
+ 
+}() );
